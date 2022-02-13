28fee1f01e31f3b4d70f4649f24144b6a322f4a9
ronaldocosta <ronaldocosta.developer@gmail.com>
2022-02-13 02:59:10 -0300

    Add collisions between ball and paddles

--- pong.py
+++ pong.py
@@ -11,7 +11,7 @@ turtle.tracer(0, 0)
 # Constants
 FPS = 60  # constant: refresh about 30 times per second
 TIMER_VALUE = 1000 // FPS  # the timer value in milliseconds for timer events
-SPEED = 500  # 100 units per second
+SPEED = 400  # 100 units per second
 
 # Variables
 ball = turtle.Turtle()  # ball of the pong game
@@ -23,6 +23,8 @@ current_left_paddle_ypos = 1  # current left y coordinate
 right_paddle = turtle.Turtle()
 current_right_paddle_xpos = 350  # current right paddle x coordinate
 current_right_paddle_ypos = 1  # current right y coordinate
+
+# Variables to control colissions
 touch_upper_wall = False
 touch_lower_wall = True
 touch_rigth_wall = False
@@ -72,8 +74,9 @@ def right_paddle_down():
         current_right_paddle_ypos = -250
 
 
-def update_position():
-    global current_ball_xpos, current_ball_ypos, touch_upper_wall, touch_lower_wall, touch_rigth_wall, touch_left_wall
+def update_ball_position():
+    global current_ball_xpos, current_ball_ypos, touch_upper_wall, touch_lower_wall, touch_rigth_wall, touch_left_wall,\
+        current_left_paddle_xpos, current_left_paddle_ypos, current_right_paddle_xpos, current_right_paddle_ypos
 
     # changes the current position of the ball based on collisions
     if touch_upper_wall and touch_rigth_wall:
@@ -101,18 +104,35 @@ def update_position():
 
     # collision with the left wall:
     if current_ball_xpos < -390:
-        touch_left_wall = True
+        current_ball_xpos = 1
+        current_ball_ypos = 1
+        touch_upper_wall = False
+        touch_lower_wall = True
         touch_rigth_wall = False
+        touch_left_wall = True
 
     # collision with the righ wall:
     if current_ball_xpos > 390:
+        current_ball_xpos = 1
+        current_ball_ypos = 1
+        touch_upper_wall = False
+        touch_lower_wall = True
+        touch_rigth_wall = True
+        touch_left_wall = False
+
+    # collision with left paddle
+    if current_ball_xpos < -330 and current_left_paddle_ypos + 50 > current_ball_ypos > current_left_paddle_ypos - 50:
+        touch_left_wall = True
+        touch_rigth_wall = False
+
+    if current_ball_xpos > 330 and current_right_paddle_ypos + 50 > current_ball_ypos > current_right_paddle_ypos - 50:
         touch_rigth_wall = True
         touch_left_wall = False
 
 
 def update_states():
     global should_draw
-    update_position()
+    update_ball_position()
     should_draw = True
     screen.ontimer(update_states, TIMER_VALUE)
 
